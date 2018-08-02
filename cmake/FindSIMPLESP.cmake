391035c10c63afc7f3a7c23ced31b8e5b37691e5
furihata <sanko.shoko@gmail.com>
2018-08-03 00:45:13 +0900

    init

---
+++ cmake/FindSIMPLESP.cmake
@@ -0,0 +1,12 @@
+﻿# SIMPLESP_FOUND
+# SIMPLESP_INCLUDE_DIR
+#
+
+find_path (SIMPLESP_DIR NAMES simplesp.h
+    PATHS
+    	"${SIMPLESP_ROOT_DIR}"
+    	/usr/
+    	/usr/local/
+)
+set(SIMPLESP_INCLUDE_DIR ${SIMPLESP_DIR})
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include "gtmaker.h"
+
+int main() {
+
+    GTMakerGUI win;
+    win.execute("gtmaker", 1000, 600);
+
+    return 0;
+}
